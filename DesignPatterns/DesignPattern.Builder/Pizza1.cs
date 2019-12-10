42356cf7f148291d899d0368ad55fc72decfe3b7
javiermunozalonso <jamual90@gmail.com>
2019-12-10 13:09:45 +0100

    Save changes

---
+++ DesignPatterns/DesignPattern.Builder/Pizza1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPattern.Builder
+{
+    public class Pizza
+    {
+        private ICollection<string> _ingredients { get; set; }
+
+        public ICollection<string> GetIngredients()
+        {
+            return null;
+        }
+
+        public override string ToString()
+        {
+
+            if (_ingredients.Count == 0)
+            {
+                return "Pizza has no ingredients :(";
+            }
+
+            return base.ToString();
+        }
+    }
+}
